baa2cb057cc8a7fed77d888d0e51643df0978cc2
Aakanksha Kashayap <kashayap2002@gmail.com>
2023-05-29 20:29:11 +0530

    Initial commit

---
+++ payment.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Payment Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Payment Page</h1>
+
+    <form action="process_payment.php" method="POST">
+        <div class="form-group">
+            <label for="amount">Amount:</label>
+            <input type="number" id="amount" name="amount" required>
+        </div>
+
+        <div class="form-group">
+            <label for="paymentType">Payment Type:</label>
+            <select id="paymentType" name="paymentType" required>
+                <option value="">Select Payment Type</option>
+                <option value="creditcard">Credit Card</option>
+                <option value="paypal">PayPal</option>
+                <!-- Add more payment options as needed -->
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Pay Now">
+        </div>
+    </form>
+</body>
+</html>
---
+++ thank.you.php
@@ -0,0 +1,11 @@
+<!-- thank_you.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thank You</title>
+</head>
+<body>
+    <h1>Thank You for Your Donation!</h1>
+    <p>We have sent you an email with the payment confirmation.</p>
+</body>
+</html>
